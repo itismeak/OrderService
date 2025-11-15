07a890128f8c791a130fec8237cae532a5feb566
Ajithkumar <ajithkumarmsep19@gmail.com>
2025-11-15 22:00:29 +0530

    fiter modify

--- src/main/java/com/microservice/order_service/common/constants/AppConstant.java
+++ src/main/java/com/microservice/order_service/common/constants/AppConstant.java
@@ -2,4 +2,12 @@ package com.microservice.order_service.common.constants;
 
 public class AppConstant {
     public static final  String apiVersion="/api/v1";
+    public static final String[] PUBLIC_ENDPOINTS = {
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/webjars/**",
+            "/favicon.ico"
+    };
 }
